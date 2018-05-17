3248ad09f35439aea5b92e88e0b6879410ca8a02
Spector0 <www.ranjith007.ra@gmail.com>
2018-05-17 13:41:45 +0530

    Bacon: Move to dotOS

--- dot.mk
+++ dot.mk
@@ -21,7 +21,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from bacon device
 $(call inherit-product, device/oneplus/bacon/bacon.mk)
 
-PRODUCT_NAME := aosp_bacon
+#inherit some common dotOS stuff
+$(call inherit-product, vendor/dot/config/common.mk)
+
+PRODUCT_NAME := dot_bacon
 PRODUCT_DEVICE := bacon
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_MODEL := A0001
